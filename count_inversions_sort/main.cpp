3fdef9be9d2cff63e5f21d9b29677c0f6a1999ab
Mek <nik.podterob@gmail.com>
2023-02-22 21:39:11 +0300

    Initial Commit

---
+++ count_inversions_sort/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  count_inversions_sort
+//
+//  Created by Никита Подтероб on 22.02.23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
